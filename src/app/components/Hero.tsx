ed93ffb34c7d7863fdd705bdd2475dec3e063757
fajshah <161027213+fajshah@users.noreply.github.com>
2024-12-13 16:48:04 +0500

    Update Hero.tsx

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,10 +1,10 @@
 'use client';
 import Link from 'next/link';
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
+
 
 function Hero() {
-  const router = useRouter();
+ 
 
   return (
     <div className="relative w-full bg-[#F3F3F3] overflow-hidden flex flex-col md:flex-row items-center justify-between">
