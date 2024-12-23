f5dad4057db4fb2a8637e6c993e6ec2c682cce97
fajshah <161027213+fajshah@users.noreply.github.com>
2024-12-24 00:59:53 +0500

    Update 404.tsx

--- src/404.tsx
+++ src/404.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 const Custom404 = () => (
   <div className="container mx-auto py-16 px-4 bg-black text-white text-center">
     <h1 className="text-5xl font-bold mb-8">404 - Page Not Found</h1>
-<<<<<<< HEAD
+
     <p>The page you&apos;re looking for doesn&apos;&apos; &apos;&apos;&apos; exist or has been moved. &apos;</p>
-=======
+
    <p>The page you&apos;re looking for doesn&apos;&apos; &apos;&apos;&apos; exist or has been moved. &apos;</p>
->>>>>>> d67700a1d01bc48a2433f534844d2edf3c553c8f
+
     <Link href="/" className="bg-blue-500 py-2 px-6 mt-8 rounded-md hover:bg-blue-700">
       Go Home
     </Link>
